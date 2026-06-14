97d03b39d990f7b82f52ff2a5ce6c7d88868ab2b
zzz <1111>
2026-06-14 13:46:52 +0800

    Initial commit

---
+++ FocusToolkit/FocusToolkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FocusToolkit
+//
+//  Created by luosi on 14/6/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
